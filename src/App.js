ea9cb044cfe283f98bdb15b911486513e297b9ec
Manuel Caroca <manuelcaroca.everis@latam.com>
2022-09-01 02:30:45 -0400

    master(components): se cean componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoCounter } from './TodoCounter';
+import { TodoItem } from './TodoItem';
+import { TodoList } from './TodoList';
+import { TodoSearch } from './TodoSearch';
+
+const todos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Tomar el curso de React', completed: false },
+  { text: 'Llorar', completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter/>
+      <TodoSearch/>
+      <TodoList>
+        {
+          todos.map(todo => (
+            <TodoItem key={todo.text} text={todo.text} />
+          ))
+        }
+      </TodoList>
+      <CreateTodoButton/>
+    </React.Fragment>
   );
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoList(props) {
+  return (<section>
+    { props.children }
+  </section>);
+}
+
+export { TodoList };
